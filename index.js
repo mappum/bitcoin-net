3be78e6da8957cbb02bc2732de8ebfaca1af1c15
Matt Bell <mappum@gmail.com>
2016-02-25 14:05:33 -0800

    Added Bridge

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   BlockStream: require('./lib/blockStream.js'),
+  Bridge: require('./lib/bridge.js'),
   Peer: require('./lib/peer.js'),
   PeerGroup: require('./lib/peerGroup.js'),
   TransactionStream: require('./lib/transactionStream.js')
