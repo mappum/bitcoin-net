551db20c975e7bd5b6cb23d3af0a2347181ce16e
Matt Bell <mappum@gmail.com>
2016-02-25 15:08:04 -0800

    Bridge: use es6 class syntax

--- src/bridge.js
+++ src/bridge.js
@@ -1,40 +1,43 @@
-var util = require('util')
+'use strict'
+
 var debug = require('debug')('bitcoin-net:bridge')
 var PeerGroup = require('./peerGroup.js')
 
-var Bridge = module.exports = function (params, opts) {
-  opts = Object.assign({ connectWeb: false }, opts)
-  PeerGroup.call(this, params, opts)
-}
-util.inherits(Bridge, PeerGroup)
-
-Bridge.prototype._onConnection = function (err, client) {
-  if (err) {
-    this.emit('connectError', err, null)
+module.exports =
+class Bridge extends PeerGroup {
+  constructor (params, opts) {
+    opts = Object.assign({ connectWeb: false }, opts)
+    super(params, opts)
   }
-  this.emit('connection', client)
-  this._connectPeer((err, bridgePeer) => {
+
+  _onConnection (err, client) {
     if (err) {
-      this.emit('connectError', err)
-      return this._onConnection(null, client)
+      this.emit('connectError', err, null)
     }
-    var onError = (err) => {
-      client.destroy()
-      bridgePeer.destroy()
-      debug('error', err.message)
-      this.emit('peerError', err, client, bridgePeer)
-    }
-    client.once('error', onError)
-    bridgePeer.once('error', onError)
-    client.once('close', () => bridgePeer.destroy())
-    bridgePeer.once('close', () => client.destroy())
+    this.emit('connection', client)
+    this._connectPeer((err, bridgePeer) => {
+      if (err) {
+        this.emit('connectError', err)
+        return this._onConnection(null, client)
+      }
+      var onError = (err) => {
+        client.destroy()
+        bridgePeer.destroy()
+        debug('error', err.message)
+        this.emit('peerError', err, client, bridgePeer)
+      }
+      client.once('error', onError)
+      bridgePeer.once('error', onError)
+      client.once('close', () => bridgePeer.destroy())
+      bridgePeer.once('close', () => client.destroy())
 
-    client.pipe(bridgePeer).pipe(client)
-    this.emit('bridge', client, bridgePeer)
-  })
-}
+      client.pipe(bridgePeer).pipe(client)
+      this.emit('bridge', client, bridgePeer)
+    })
+  }
 
-Bridge.prototype.connect = function () {
-  // don't let consumers try to make outgoing connections
-  throw new Error('Do not use "connect()" with Bridge, only incoming connections are allowed')
+  connect () {
+    // don't let consumers try to make outgoing connections
+    throw new Error('Do not use "connect()" with Bridge, only incoming connections are allowed')
+  }
 }
