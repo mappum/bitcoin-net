3434c451d10838427b2d875c2068e34a074eae8e
Matt Bell <mappum@gmail.com>
2016-02-29 14:56:01 -0800

    Added connect/disconnect tests for PeerGroup

--- test/peerGroup.js
+++ test/peerGroup.js
@@ -29,3 +29,42 @@ test('PeerGroup constructor', (t) => {
 
   t.end()
 })
+
+test('connect', (t) => {
+  // NOTE: this test connects to real nodes
+  var numPeers = 8
+  var peers = new PeerGroup(params, { numPeers })
+
+  t.test('connect to peers', (t) => {
+    var onPeer = (peer) => {
+      t.ok(peer, 'got peer')
+      t.ok(peer.ready, 'peer has completed handshake')
+      t.equal(peer.magic, params.magic, 'correct magic bytes')
+      t.ok(peer.socket.readable, 'peer transport socket is readable')
+      t.ok(peer.socket.writable, 'peer transport socket is writable')
+      if (peers.peers.length < numPeers) return
+      t.pass('connected to `numPeers` peers')
+      peers.removeListener('peer', onPeer)
+      t.end()
+    }
+    peers.on('peer', onPeer)
+    peers.connect()
+    t.ok(peers.connecting, 'peer "connecting" state is now true')
+  })
+
+  t.test('disconnect from peers', (t) => {
+    var i = 0
+    peers.on('disconnect', (peer) => {
+      i++
+      t.ok(peer, 'got disconnected peer')
+      t.equal(peers.peers.length, numPeers - i, 'correct number of remaining peers')
+      if (i < numPeers) return
+      t.equal(peers.peers.length, 0, 'disconnected from all peers')
+      t.end()
+    })
+    peers.disconnect()
+    t.notOk(peers.connecting, 'peer "connecting" state is now true')
+  })
+
+  t.end()
+})
