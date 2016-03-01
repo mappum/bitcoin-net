a13997a3fa21896e4db1c17e12556c1de29736cc
Matt Bell <mappum@gmail.com>
2016-03-01 14:46:12 -0800

    Added tests for PeerGroup-created streams

--- test/peerGroup.js
+++ test/peerGroup.js
@@ -1,10 +1,13 @@
 var test = require('tap').test
-var params = require('webcoin-bitcoin').net
+var params = require('webcoin-bitcoin')
 var u = require('bitcoin-util')
 var Block = require('bitcoinjs-lib').Block
+var Blockchain = require('blockchain-spv')
+var levelup = require('levelup')
+var memdown = require('memdown')
 var PeerGroup = require('../lib/peerGroup.js')
 var HeaderStream = require('../lib/headerStream.js')
-// var BlockStream = require('../lib/blockStream.js')
+var BlockStream = require('../lib/blockStream.js')
 
 test('PeerGroup constructor', (t) => {
   t.test('invalid params', (t) => {
@@ -20,13 +23,13 @@ test('PeerGroup constructor', (t) => {
   })
 
   t.test('valid params, no options', (t) => {
-    var peers = new PeerGroup(params)
+    var peers = new PeerGroup(params.net)
     t.ok(peers, 'created PeerGroup')
     t.end()
   })
 
   t.test('valid params, with options', (t) => {
-    var peers = new PeerGroup(params, { numPeers: 4 })
+    var peers = new PeerGroup(params.net, { numPeers: 4 })
     t.ok(peers, 'created PeerGroup')
     t.end()
   })
@@ -37,13 +40,13 @@ test('PeerGroup constructor', (t) => {
 test('connect, disconnect', (t) => {
   // NOTE: this test connects to real nodes
   var numPeers = 8
-  var peers = new PeerGroup(params, { numPeers })
+  var peers = new PeerGroup(params.net, { numPeers })
 
   t.test('connect to peers', (t) => {
     var onPeer = (peer) => {
       t.ok(peer, 'got peer')
       t.ok(peer.ready, 'peer has completed handshake')
-      t.equal(peer.magic, params.magic, 'correct magic bytes')
+      t.equal(peer.magic, params.net.magic, 'correct magic bytes')
       t.ok(peer.socket.readable, 'peer transport socket is readable')
       t.ok(peer.socket.writable, 'peer transport socket is writable')
       if (peers.peers.length < numPeers) return
@@ -75,7 +78,7 @@ test('connect, disconnect', (t) => {
 
 test('peer methods', (t) => {
   var numPeers = 4
-  var pg = new PeerGroup(params, { numPeers })
+  var pg = new PeerGroup(params.net, { numPeers })
 
   t.test('connect', (t) => {
     var onPeer = () => {
@@ -101,41 +104,69 @@ test('peer methods', (t) => {
     t.end()
   })
 
+  var chain
+  t.test('setup blockchain', (t) => {
+    var db = levelup('chain', { db: memdown })
+    var blockchainParams = Object.assign({}, params.blockchain)
+    blockchainParams.checkpoints = null
+    chain = new Blockchain(blockchainParams, db)
+    chain.once('ready', () => t.end())
+  })
+
   t.test('createHeaderStream', (t) => {
-    var start = u.toHash('000000000000679c158c35a47eecb6352402baeedd22d0385b7c9d14a922f218')
-    var stream = pg.createHeaderStream({ locator: [ start ] })
-    t.ok(stream instanceof HeaderStream, 'got HeaderStream')
     var expectedHeaders = [
       {
-        first: u.toHash('0000000000011d5533cc761e36eab351a92593dfd16c4c16b9076a4928c5c864'),
-        last: u.toHash('00000000000051ffb3a39684c657ee29c8006489bfc3b33dc1a037352f79c784')
+        first: u.toHash('00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048'),
+        last: u.toHash('00000000dfd5d65c9d8561b4b8f60a63018fe3933ecb131fb37f905f87da951a')
       },
       {
-        first: u.toHash('000000000000b69e1df6199421f2b3cd942f29cd55bc83bf904c9be47422d2a4'),
-        last: u.toHash('000000000000955887f2bbbeaf23dfa1f619ea84a8a2c81a2bf490e6c5bff25f')
+        first: u.toHash('0000000067217a46c49054bad67cda2da943607d326e89896786de10b07cb7c0'),
+        last: u.toHash('00000000922e2aa9e84a474350a3555f49f06061fd49df50a9352f156692a842')
       },
       {
-        first: u.toHash('000000000000f270fa104d4acb8916efdedc3e926ebb40165e232e65ec8e1468'),
-        last: u.toHash('000000000000af3fa8e1453ba3f16e2e2a6e38c40f76a8b84364d5129b042de6')
+        first: u.toHash('00000000a86f68e8de06c6b46623fdd16b7a11ad9651fa48ecbe8c731658dc06'),
+        last: u.toHash('00000000dbbb79792303bdd1c6c4d7ab9c21bba0667213c2eca955e11230c5a5')
       },
       {
-        first: u.toHash('0000000000007adc528d0c9bcaa8b59d4d74731b197e74f0245edcf3821e1d82'),
-        last: u.toHash('0000000000008cc7817dcbfc514f0ab3ccca36fbdc62f7f4560a3e28bd05b886')
+        first: u.toHash('0000000055fcaf04cb9a82bb86b46a21b15fcaa75ac8c18679b0234f79c4c615'),
+        last: u.toHash('0000000094fbacdffec05aea9847000522a258c269ae37a74a818afb96fc27d9')
       }
     ]
+    var start = u.toHash('000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')
+    var stream = pg.createHeaderStream({ locator: [ start ] })
+    t.ok(stream instanceof HeaderStream, 'got HeaderStream')
     stream.on('data', (data) => {
       t.ok(Array.isArray(data), 'data is array')
       t.equal(data.length, 2000, 'data is correct length')
+      t.ok(data[0] instanceof Block, 'data contains block headers (bitcoinjs Block)')
       var expected = expectedHeaders.shift()
-      console.log(data[0].getId(), expected.first.toString('hex'))
       t.ok(data[0].getHash().compare(expected.first) === 0, 'got correct first header')
       t.ok(data[1999].getHash().compare(expected.last) === 0, 'got correct last header')
-      if (expectedHeaders.length === 0) {
-        stream.end()
-        t.end()
-      }
+      chain.addHeaders(data, (err) => {
+        t.error(err, 'headers add to blockchain')
+        if (expectedHeaders.length === 0) {
+          stream.end()
+          t.end()
+        }
+      })
     })
   })
+
+  // t.test('createBlockStream', (t) => {
+  //   var stream = pg.createBlockStream(chain, { from: chain.genesis.hash })
+  //   t.ok(stream instanceof BlockStream, 'got BlockStream')
+  //   var lastHeight = 0
+  //   var lastHash = chain.genesis.hash
+  //   stream.on('data', (data) => {
+  //     t.equal(typeof data.height, 'number', 'data contains height')
+  //     t.ok(data.header instanceof Block, 'data contains header (bitcoinjs Block)')
+  //     t.equal(data.height, lastHeight + 1, 'blocks ordered by height')
+  //     t.ok(data.header.prevHash.compare(lastHash) === 0, 'block connects to previous hash')
+  //     lastHeight++
+  //     lastHash = data.header.getHash()
+  //   })
+  //   stream.on('end', () => t.end())
+  // })
 
   t.test('disconnect', (t) => {
     pg.on('disconnect', () => {
