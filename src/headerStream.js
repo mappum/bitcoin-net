f95946dc1fb901ae6b82b05ad7d5ae4b47074d41
Matt Bell <mappum@gmail.com>
2016-03-02 16:40:01 -0800

    HeaderStream: use PeerGroup instead of peer

--- src/headerStream.js
+++ src/headerStream.js
@@ -1,23 +1,18 @@
 var Readable = require('stream').Readable
 var util = require('util')
-var u = require('bitcoin-util')
 
-var HeaderStream = module.exports = function (peer, opts) {
-  if (!peer) throw new Error('"peer" option is required for HeaderStream')
+var HeaderStream = module.exports = function (peers, opts) {
+  if (!peers) throw new Error('"peers" option is required for HeaderStream')
   Readable.call(this, { objectMode: true, highWaterMark: 4 })
   opts = opts || {}
-  this.peer = peer
+  this.peers = peers
   this.locator = opts.locator || []
+  this.timeout = opts.timeout
   this.stop = opts.stop
-  this.disconnected = false
   this.getting = false
   this.done = false
 
-  this._onDisconnect = this._onDisconnect.bind(this)
-  this.peer.on('disconnect', this._onDisconnect)
-
   this._onHeaders = this._onHeaders.bind(this)
-  this.peer.on('headers', this._onHeaders)
 }
 util.inherits(HeaderStream, Readable)
 
@@ -26,38 +21,30 @@ HeaderStream.prototype._error = function (err) {
   this.end()
 }
 
-HeaderStream.prototype._onDisconnect = function () {
-  this.disconnected = true
-  this._error(new Error('Disconnected from peer'))
-}
-
 HeaderStream.prototype._read = function () {
   this._getHeaders()
 }
 
 HeaderStream.prototype.end = function () {
   this.done = true
-  this.peer.removeListener('disconnect', this._onDisconnect)
-  this.peer.removeListener('headers', this._onHeaders)
   this.push(null)
 }
 
 HeaderStream.prototype._getHeaders = function () {
-  if (this.disconnected || this.getting || this.done) return
+  if (this.getting || this.done) return
   this.getting = true
-  this.peer.send('getheaders', {
-    version: this.peer.protocolVersion,
-    locator: this.locator,
-    hashStop: this.stop || u.nullHash
-  })
-  // TODO: timeout if we don't get a response
+  this.peers.getHeaders(this.locator, {
+    stop: this.stop,
+    timeout: this.timeout
+  }, this._onHeaders)
 }
 
-HeaderStream.prototype._onHeaders = function (message) {
+HeaderStream.prototype._onHeaders = function (err, headers) {
+  if (err) return this._error(err)
   this.getting = false
-  if (message.length === 0) return this.end()
-  this.locator = [ message[message.length - 1].getHash() ]
-  var res = this.push(message)
-  if (message.length < 2000) return this.end()
+  if (headers.length === 0) return this.end()
+  this.locator = [ headers[headers.length - 1].getHash() ]
+  var res = this.push(headers)
+  if (headers.length < 2000) return this.end()
   if (res) this._getHeaders()
 }
