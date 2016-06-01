13f6fcc082effbfe0e7127b2f2ff14d1d4fb4026
Matt Bell <mappum@gmail.com>
2016-06-01 03:00:40 -0700

    Don't export streams

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = {
-  BlockStream: require('./lib/blockStream.js'),
   Bridge: require('./lib/bridge.js'),
   Peer: require('./lib/peer.js'),
-  PeerGroup: require('./lib/peerGroup.js'),
-  TransactionStream: require('./lib/transactionStream.js')
+  PeerGroup: require('./lib/peerGroup.js')
 }
