035c78682c526de26e08dda8fb5932024ebb5bb3
Felix Schuchmann <schuchmann@gmail.com>
2016-07-06 15:46:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  Transforms: require('./src/protocolTransforms.js'),
   Bridge: require('./src/bridge.js'),
   Peer: require('./src/peer.js'),
   PeerGroup: require('./src/peerGroup.js')
